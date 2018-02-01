689538922ed53cd312ab9a7a66cf8fbb53d24d71
sihammeet-18 <siham.abuali12@gmail.com>
2018-02-01 17:43:24 +0200

    Im done

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request
 from sqlalchemy import create_engine 
 from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///./test.db'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///./test.db'  
+
+import views
+
 
 db = SQLAlchemy(app)
 # concerns=[]
